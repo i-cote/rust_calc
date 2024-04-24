60eb90548bc2b8d0e228dc5719ea18b65b22492a
Irvin Cote <irvincoteg@gmail.com>
2024-04-24 21:34:05 +0300

    adding tests

--- src/automata.rs
+++ src/automata.rs
@@ -44,3 +44,96 @@ impl <'a>Automata<'a> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_compute() {
+        let (expr, expected) = (String::from("1+22*4*4+(1+2)*3"), 1+22*4*4+(1+2)*3);
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+
+    #[test]
+    fn test_compute1() {
+        let (expr, expected) = 
+        
+            (String::from("5+12+4*3+2*3"), 5+12+4*3+2*3)
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+
+    #[test]
+    fn test_compute2() {
+        let (expr, expected) = 
+        
+            (String::from("3*3+1*(10*2*3)"), 3*3+1*(10*2*3))
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_compute3() {
+        let (expr, expected) = 
+        
+            (String::from("((15*4))+3*(22)"), ((15)*4)+3*(22))
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_compute4() {
+        let (expr, expected) = 
+        
+            (String::from("100*(5*2)+3"), 100*(5*2)+3)
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_compute5() {
+        let (expr, expected) = 
+        
+            (String::from("2*(3+5*2)+4"), 2*(3+5*2)+4)
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_compute6() {
+        let (expr, expected) = 
+        
+            (String::from("(4*4+3*3)*5"), (4*4+3*3)*5)
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+
+    #[test]
+    fn test_compute7() {
+        let (expr, expected) = 
+        
+            (String::from("7*5+20+(4+1)"), 7*5+20+(4+1))
+        ; 
+        let mut automata = Automata::new(&expr);
+        let result = automata.compute();
+        assert_eq!(result.unwrap(), expected);
+    }
+    
+}
